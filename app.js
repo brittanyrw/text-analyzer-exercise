1d8b0d3696b54726fe878b46420ee5cee3bb3897
Brittany Walker <breneewalker@gmail.com>
2017-01-25 18:37:54 -0500

    update

--- app.js
+++ app.js
@@ -17,48 +17,44 @@ $(document).ready(function() {
 		$(".js-word-count").text(userInputWordsArray.length);
 		$(".js-unique-word-count").text(uniqueWordCount(userInputWordsArray));
 		$(".js-average-word-length").text(avgWordLength(userInputWordsArray) + " characters");
-		$(".js-average-sentence-length").text(avgSentenceLength(userInputSentenceArray) + " words");
+		$(".js-average-sentence-length").text(avgSentenceLength(userInputSentenceArray) + " characters");
 
 		//remove hidden class to display text analyzer results
 		$('.text-report').removeClass('hidden'); 
 	});
+
+});
 
 //Functions for analyzing the text
 
 
 function uniqueWordCount(userInputWordsArray) {
 	var wordCountObj = {};
-	/*for (var i = 0; i < userInputWordsArray.length; i++) {
+	for (var i = 0; i < userInputWordsArray.length; i++) {
 		if (userInputWordsArray[i] in wordCountObj) {
 			wordCountObj[userInputWordsArray[i]] += 1;
 		} else {
 			wordCountObj[userInputWordsArray[i]] = 1;
 		}
-	}*/
-for (var i = 0; i < userInputWordsArray.length; i ++) {
-	var uniqueWordCountArray = [];
-	if (!(userInputWordsArray[i] in wordCountObj)) {
-		uniqueWordCountArray.push(userInputWordsArray[i])
 	}
+		return Object.keys(wordCountObj).length;
 }
-	return uniqueWordCountArray.length;
-	/*return Object.keys(wordCountObj).length;*/
-};
 
 function avgWordLength(userInputWordsArray) {
 	var avgWordLength = userInputWordsArray.join("").length;
 	return (avgWordLength / userInputWordsArray.length).toFixed(2);
-};
+}
 
 function avgSentenceLength(userInputSentenceArray) {
 
-	var numberOfSentences = userInputSentenceArray.length;
+	var numberOfSentences = (userInputSentenceArray.length) -1;
 	var sentenceLengthTotal = 0;
 	for (var i = 0; i < userInputSentenceArray.length; i++) {
 		if (userInputSentenceArray[i] != "") {
 			sentenceLengthTotal += userInputSentenceArray[i].length;
 		}
 		console.log(userInputSentenceArray);
+		console.log(numberOfSentences);
 	} 
 	return (sentenceLengthTotal/numberOfSentences).toFixed(2);
 };
